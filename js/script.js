71a4ced0377769a242a7d3b68e7fad121ef5ef47
Antares-ProgramIng <140986762+Antares-ProgramIng@users.noreply.github.com>
2024-05-17 17:25:21 -0300

    style changes inicio

--- js/script.js
+++ js/script.js
@@ -1,5 +1,3 @@
-
-
 const INICIOBTN = document.getElementById("Inicio_BTN")
 const CURSOSBTN = document.getElementById("Cursos_BTN")
 const SERVICIOSBTN = document.getElementById("Servicios_BTN")
@@ -22,18 +20,116 @@ function activebtn(btn){
     btn.classList.add("ciep_BTN_active")
 }
 
+
+
+//funcion que agrega los event listeners a los botones de Mision Vision y valores en el inicio.
+function addMVVEventListeners() {
+    const MISIONCARD = document.getElementById("mision_card");
+    const MISIONCONTENT = document.getElementById("mision_content");
+    const VISIONCARD = document.getElementById("vision_card");
+    const VISIONCONTENT = document.getElementById("vision_content");
+    const VALORESCARD = document.getElementById("valores_card");
+    const VALORESCONTENT = document.getElementById("valores_content");
+
+    MISIONCARD.addEventListener("click", () => {
+        MISIONCONTENT.classList.add("active");
+        VISIONCONTENT.classList.remove("active");
+        VALORESCONTENT.classList.remove("active");
+        
+        MISIONCARD.classList.add("active");
+        VISIONCARD.classList.remove("active");
+        VALORESCARD.classList.remove("active");
+    });
+
+    VISIONCARD.addEventListener("click", () => {
+        VISIONCONTENT.classList.add("active");
+        MISIONCONTENT.classList.remove("active");
+        VALORESCONTENT.classList.remove("active");
+        
+        VISIONCARD.classList.add("active");
+        MISIONCARD.classList.remove("active");
+        VALORESCARD.classList.remove("active");
+    });
+
+    VALORESCARD.addEventListener("click", () => {
+        VALORESCONTENT.classList.add("active");
+        MISIONCONTENT.classList.remove("active");
+        VISIONCONTENT.classList.remove("active");
+        
+        VALORESCARD.classList.add("active");
+        MISIONCARD.classList.remove("active");
+        VISIONCARD.classList.remove("active");
+    });
+    VISIONCARD.click();
+}
+
+//aqui agregamos un event listener al doc para que haga click en el boton de inicio al iniciar la pagina.
+document.addEventListener("DOMContentLoaded", (event) => {
+    INICIOBTN.click();
+});
+
+//boton de inicio, lo que muestra en el contenido dinamico al presionar el boton de inicio
 INICIOBTN.addEventListener("click", ()=>{
     activebtn(INICIOBTN)
     DINAMICCONTENT.innerHTML = ``;
     DINAMICCONTENT.innerHTML = `
-    <h2 class="titulos">Bienvenidos al Centro Académico</h2>
-    <p class="infos">Somos una institución comprometida con la excelencia educativa y el desarrollo integral de nuestros estudiantes.</p>
-    <p class="infos">Nuestra misión es proporcionar una educación de calidad que promueva el aprendizaje significativo y la formación de ciudadanos responsables.</p>
-    <p class="infos">Visión: Ser líderes en la innovación educativa y contribuir al progreso de nuestra sociedad.</p>
-    `;
+    <div id="inicio_content">
 
+                    <div class="content_title_container">
+                        <h2 class="titulos">Bienvenidos al Centro Académico CIEP.</h2>
+                    </div>
+
+                    <div class="left_img_container">
+                        <img class="left_content_img" src="./images/Welcome_img.jpeg" alt="">
+                        <p class="infos left_content_p">Somos una institución comprometida con la excelencia educativa y el desarrollo integral de nuestros estudiantes. Creemos firmemente en la importancia de proporcionar una educación de calidad que no solo transmita conocimientos académicos, sino que también fomente habilidades críticas, creatividad y valores éticos. Nuestro enfoque se centra en el estudiante, promoviendo un ambiente de aprendizaje inclusivo y estimulante que permite a cada individuo alcanzar su máximo potencial. Además, nos esforzamos por desarrollar en nuestros alumnos una conciencia global y un compromiso social, preparándolos para enfrentar los desafíos del mundo moderno con responsabilidad y proactividad.</p>    
+                    </div>
+                    
+                    <div id="MVV_container">
+                        <div class="card_group_container">
+
+                            <div class="MVV_card" id="mision_card">
+                                <img src="images/Mision_icon.png" class="MVV_img" alt="Curso 1">
+                                <div class="MVV-body">
+                                    <p class="MVV_title">Misión</p>
+                                    <p class="MVV_content" id="mision_content">Proporcionar una educación de calidad que promueva el aprendizaje significativo y la formación de ciudadanos responsables. Nos dedicamos a crear un entorno educativo donde cada estudiante pueda explorar su potencial y desarrollar habilidades críticas, analíticas y creativas. Valoramos la importancia de un currículo integral que no solo aborde las áreas académicas tradicionales, sino que también incluya la educación en competencias transversales. Creemos que un aprendizaje significativo va más allá de la adquisición de conocimientos; implica la capacidad de aplicar lo aprendido en contextos reales, fomentando la resolución de problemas, la innovación y el pensamiento independiente.</p>
+                                </div>
+                            </div>
+    
+                            <div class="MVV_card" id="vision_card">
+                                <img src="./images/Vision_icon.png" class="MVV_img" alt="Curso 2">
+                                <div class="MVV-body">
+                                    <p class="MVV_title">vision</p>
+                                    <p class="MVV_content" id="vision_content">Lorem ipsum dolor sit amet consectetur adipiscing elit aliquam felis, viverra quisque nascetur malesuada at ad lectus tortorfermentum torquent per non aliquet nullam eu, fusce justo etiam purus hac at rutrum scelerisque iaculis imperdiet pharetra. Lacus justo bibendum senectus porta commodo lobortis nulla mattis habitasse sodales, dui habitant pharetra non primis fringilla netus ultrices mus, parturient aliquam curae magnis torquent dignissim potenti dis augue.</p>
+                                </div>
+                            </div>
+    
+                            <div class="MVV_card" id="valores_card">
+                                <img src="./images/Valores_icon.png" class="MVV_img" alt="Curso 3">
+                                <div class="MVV-body">
+                                    <p class="MVV_title">Valores<p>
+                                    <p class="MVV_content" id="valores_content">Lorem ipsum dolor sit amet consectetur adipiscing elit aliquam felis, viverra quisque nascetur malesuada at ad lectus tortor ligula suscipit, ridiculus ac faucibus vel risus vehicula metus fusce justo etiam purus hac at rutrum scelerisque iaculis imperdiet pharetra. Lacus justo bibendum senectus porta commodo lobortis nulla mattis habitasse sodales, dui habitant pharetra non primis fringilla netus ultrices mus, parturient aliquam curae magnis torquent dignissim potenti dis augue.</p>
+                                </div>
+                            </div>
+    
+                        </div>
+                    </div>
+
+                    <div class="left_img_container">
+                        <p class="infos left_content_p">Lorem ipsum dolor sit amet consectetur adipiscing elit aliquam felis, viverra quisque nascetur malesuada at ad lectus tortor ligula suscipit, ridiculus ac faucibus vel risus vehicula metus dictumst. Massa tincidunt enim proin eleifend malesuada fermentum torquent per non aliquet nullam eu, fusce justo etiam purus hac at rutrum scelerisque iaculis imperdiet pharetra. Lacus justo bibendum senectus porta commodo lobortis nulla mattis habitasse sodales, dui habitant pharetra non primis fringilla netus ultrices mus, parturient aliquam curae magnis torquent dignissim potenti dis augue.</p>    
+                    
+                        <img class="left_content_img" src="./images/right_img.png" alt="">
+                    </div>
+
+                    <div class="little_div">
+
+                    </div>
+
+                </div>
+    `;
+    addMVVEventListeners()
 })
 
+//boton de cursos, lo que muestra en el contenido dinamico al presionar el boton de cursos
 CURSOSBTN.addEventListener("click", ()=>{
     activebtn(CURSOSBTN)
     DINAMICCONTENT.innerHTML = ``;
@@ -68,6 +164,7 @@ CURSOSBTN.addEventListener("click", ()=>{
 `;
 })
 
+//boton de Servicios, lo que muestra en el contenido dinamico al presionar el boton de servicios
 SERVICIOSBTN.addEventListener("click", ()=>{
     activebtn(SERVICIOSBTN)
     DINAMICCONTENT.innerHTML = ``;
@@ -79,6 +176,8 @@ SERVICIOSBTN.addEventListener("click", ()=>{
     `;
 })
 
+
+//boton de instituciones, lo que muestra en el contenido dinamico al presionar el boton de instituciones
 INSTITUCIONBTN.addEventListener("click", ()=>{
     activebtn(INSTITUCIONBTN)
     DINAMICCONTENT.innerHTML = ``;
@@ -89,6 +188,7 @@ INSTITUCIONBTN.addEventListener("click", ()=>{
     `;
 })
 
+//boton de colaboradores, lo que muestra en el contenido dinamico al presionar el boton de colaboradores
 COLABORADORESBTN.addEventListener("click", ()=>{
     activebtn(COLABORADORESBTN)
     DINAMICCONTENT.innerHTML = ``;
@@ -103,6 +203,8 @@ COLABORADORESBTN.addEventListener("click", ()=>{
 `;
 })
 
+
+//boton de contacto, lo que muestra en el contenido dinamico al presionar el boton de contacto
 CONTACTOBTN.addEventListener("click", ()=>{
     activebtn(CONTACTOBTN)
     DINAMICCONTENT.innerHTML = ``;
